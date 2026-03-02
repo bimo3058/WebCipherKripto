5fcfdaeec8c7b7c30f6381967f348ae6cce8c2d7
bimo3058 <bimo3058@gmail.com>
2026-03-02 19:16:07 +0700

    Perubahan 2(1)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import AppDesktop from "./Appdesktop";
 import AppMobile from "./Appmobile";
 
-
 export default function App() {
   const isMobile = useMediaQuery("(max-width:768px)");
   return isMobile ? <AppMobile /> : <AppDesktop />;
